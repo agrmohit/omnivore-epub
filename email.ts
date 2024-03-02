b28456683cf6079889977fd0814f98f7563104b5
Mohit Raj <mohit@agrmohit.com>
2024-03-02 16:53:02 +0530

    feat: check ebook size before sending and show warning

--- email.ts
+++ email.ts
@@ -23,12 +23,7 @@ export async function sendEmail() {
     ],
   };
 
-  if (
-    await Deno.stat(config.outputFileName).catch((_err) => {
-      console.error(`🚫 ebook file '${config.outputFileName}' is missing`);
-      Deno.exit(1);
-    })
-  ) {
+  if (await verifyEpub()) {
     try {
       console.log(`📧 Sending email from '${config.emailFrom} <${config.emailUser}>' to '${config.emailRecipient}'`);
       const info = await transporter.sendMail(mailOptions);
@@ -38,4 +33,31 @@ export async function sendEmail() {
       Deno.exit(1);
     }
   }
+}
+
+async function verifyEpub(): Promise<boolean> {
+  try {
+    const file = await Deno.stat(config.outputFileName);
+
+    // Check if it is indeed a file
+    if (!file.isFile) {
+      console.error(`🚫 ${config.outputFileName} is not a file`);
+      Deno.exit(1);
+    }
+
+    // Convert from bytes to MB (not MiB) rounded off to 2 digits after decimal
+    const ebookSize = (file.size / 1_000_000).toFixed(2);
+
+    // Show a warning if ebook is over a specified size
+    if (!config.emailSizeWarningSuppress && Number(ebookSize) >= config.emailSizeWarningMinSize) {
+      console.warn(`⚠️ ebook size is too large at ${ebookSize} MB (limit: ${config.emailSizeWarningMinSize} MB)`);
+      console.warn("⚠️ Many email providers and eReader emailing services may reject this email");
+      console.warn("⚠️ To suppress this warning, set 'emailSuppressSizeWarning' to true in 'config.json'");
+    }
+  } catch (_err) {
+    console.error(`🚫 ebook file '${config.outputFileName}' is missing`);
+    Deno.exit(1);
+  }
+
+  return true;
 }
